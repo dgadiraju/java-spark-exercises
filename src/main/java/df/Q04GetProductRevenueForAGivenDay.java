ec2f99fd456d2616403157905ccd8221dacf8a85
Durga Viswanatha Raju Gadiraju <dgadiraju@ITVersity-Official.local>
2019-05-08 14:13:35 +0530

    Updated some of the java programs with instructions

--- src/main/java/df/Q04GetProductRevenueForAGivenDay.java
+++ src/main/java/df/Q04GetProductRevenueForAGivenDay.java
@@ -15,10 +15,13 @@ public class Q04GetProductRevenueForAGivenDay {
 				.getOrCreate();
 		
 		// Read Orders, Order Items and Products Data
+		// Input base directory should be passed as argument
 		
 		// Process using Data Frame Operations
+		// Filter for complete or closed orders as well as the passed date
 		
 		// Write Output into Parquet file format with out compression
+		// Output directory should be passed as argument
 		// Output should contain Product Name and Revenue in descending order by revenue
 		
 
